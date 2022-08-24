9b8b840846721e026f251ee9e6f47dc1002d9ff4
Gerald.okeke <okekegerald@gmail.com>
2022-08-24 13:33:53 +0100

    run kubernetes locally

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=geedino07/udacityproj:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacityproj4 --image=geedino07/udacityproj:v1.0.0
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udacityproj4 5001:80
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+docker tag udacityproj geedino07/udacityproj:v1.0.0
+dockerpath=geedino07/udacityproj:v1.0.0
 
 # Step 2:  
 # Authenticate & tag
+#docker tag udacityproj geedino07/udacityproj:v1.0.0
+docker login
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
